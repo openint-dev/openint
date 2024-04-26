719c89e49b85926ba782853b52b5967598809537
Tony Xiao <tonyx.ca@gmail.com>
2024-04-26 01:08:13 -0700

    chore: removing supabase proxied graphql and rest pages

--- apps/web/app/(admin)/(authenticated)/api-access/page.tsx
+++ apps/web/app/(admin)/(authenticated)/api-access/page.tsx
@@ -1,7 +1,5 @@
-import Link from 'next/link'
 import {kApikeyHeader, kApikeyUrlParam} from '@openint/app-config/constants'
 import {Input, Label} from '@openint/ui/shadcn'
-import {cn} from '@/lib-client/ui-utils'
 import {getOrCreateApikey} from '@/lib-server'
 import {serverComponentGetViewer} from '@/lib-server/server-component-helpers'
 
@@ -12,24 +10,7 @@ export default async function ApiKeyPage() {
   return (
     <div className="p-6">
       <h2 className="mb-4 text-2xl font-semibold tracking-tight">API</h2>
-      {/* TODO: Need component to display information */}
 
-      <div className="flex gap-4">
-        <Link
-          href="/api-access/graphql"
-          className={cn(
-            'text-sm font-medium transition-colors hover:text-primary',
-          )}>
-          GraphQL Explorer
-        </Link>
-        <Link
-          href="/api-access/rest"
-          className={cn(
-            'text-sm font-medium transition-colors hover:text-primary',
-          )}>
-          Rest Explorer
-        </Link>
-      </div>
       <div className="mt-4 flex items-center">
         <Label className="mr-4 shrink-0" htmlFor="apikey">
           API Key
