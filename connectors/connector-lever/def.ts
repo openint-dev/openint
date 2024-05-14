4781db36091da221e05d0fe0252c916ff19adffd
jatin <sandilya.jatin@gmail.com>
2024-05-14 12:23:42 +0530

    chore: add nango provider

--- connectors/connector-lever/def.ts
+++ connectors/connector-lever/def.ts
@@ -29,6 +29,7 @@ export const leverDef = {
     stage: 'beta',
     categories: ['ats'],
     logoUrl: '/_assets/logo-lever.png',
+    nangoProvider: 'lever',
   },
 } satisfies ConnectorDef<typeof leverSchemas>
 
