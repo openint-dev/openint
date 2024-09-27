8d5fb3061d490a1471803652c843ab0d004b6bf9
Tony Xiao <tonyx.ca@gmail.com>
2024-09-27 08:25:33 -0700

    chore: document dynamic component

--- apps/web/app/connect/portal/page.tsx
+++ apps/web/app/connect/portal/page.tsx
@@ -1,5 +1,6 @@
 // import {clerkClient} from '@clerk/nextjs/server'
 // import Image from 'next/image'
+// import nextDynamic from 'next/dynamic'
 import {kAccessToken} from '@openint/app-config/constants'
 import {getViewerId} from '@openint/cdk'
 import {zConnectPageParams} from '@openint/engine-backend/router/endUserRouter'
@@ -21,6 +22,14 @@ export const dynamic = 'force-dynamic'
 // Should we allow page to optionally load without token for performance then add token async
 // Perhaps it would even be an advantage to have the page simply be static?
 // Though that would result in waterfall loading of integrations
+
+// Workaround for hydration issues by completely disabling SSR for now...
+// except it does not work because next/dynamic only works on client components
+// https://stackoverflow.com/a/57173209/69249`9
+// const DynamicConnectionPortal = nextDynamic(
+//   () => Promise.resolve(ConnectionPortal),
+//   {ssr: false},
+// )
 
 /**
  * Embed this fully featured page to allow customers to fully manage their own resources
