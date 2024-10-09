c942c0519538a73987ca14fee9dc6663cc3ee2d1
Amadeo Pellicce <pellicceama@users.noreply.github.com>
2024-10-09 07:59:35 +0200

    adding info card for greenhouse api key (#33)

--- packages/engine-frontend/hocs/WithConnectorConnect.tsx
+++ packages/engine-frontend/hocs/WithConnectorConnect.tsx
@@ -2,7 +2,7 @@
 
 import NangoFrontend from '@nangohq/frontend'
 import {useMutation} from '@tanstack/react-query'
-import {Loader2} from 'lucide-react'
+import {InfoIcon, Loader2} from 'lucide-react'
 import React from 'react'
 import type {Id} from '@openint/cdk'
 import {CANCELLATION_TOKEN, extractId, oauthConnect} from '@openint/cdk'
@@ -197,7 +197,29 @@ export const WithConnectorConnect = ({
 
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Connect to {ccfg.connector.displayName}</DialogTitle>
+          <DialogTitle>
+            <div className="flex items-center">
+              <span className="mr-2">Connect to {ccfg.connector.displayName}</span>
+              {ccfg.connector.name === 'greenhouse' && (
+                <div className="group relative inline-block">
+                <InfoIcon className="h-5 w-5 cursor-help text-gray-500" />
+                <div className="absolute bottom-full left-1/2 mb-2 w-64 -translate-x-1/2 rounded-md bg-gray-800 p-2 text-sm text-white opacity-0 transition-opacity group-hover:opacity-100">
+                  <p className="italic">
+                    Generate a custom API key with{' '}
+                    <a
+                      href="https://support.greenhouse.io/hc/en-us/articles/202842799-Create-an-API-key-in-Greenhouse-Recruiting"
+                      className="font-bold underline"
+                      target="_blank"
+                      rel="noopener noreferrer">
+                      these instructions
+                    </a>{' '}
+                    and include all Harvest V3 permissions.
+                  </p>
+                </div>
+              </div>
+            )}
+            </div>
+          </DialogTitle>
           {debug && (
             <DialogDescription>
               Using connector config ID: {ccfg.id}
