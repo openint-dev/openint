cd656a432b9bb733ba05fb1f7e83af2faacada53
Amadeo Pellicce <pellicceama@gmail.com>
2024-10-08 17:50:35 -0700

    WIP google connector

--- connectors/connector-google/server.ts
+++ connectors/connector-google/server.ts
@@ -1,36 +1,37 @@
-// import type {HubspotSDK} from '@opensdks/sdk-google'
+import {initGoogleSDK} from '@opensdks/sdk-google'
 import type {ConnectorServer} from '@openint/cdk'
 import type {googleSchemas} from './def'
 
 export const googleServer = {
-  // newInstance: ({settings, fetchLinks}) => {
-  //   const sdk = initHubspotSDK({
-  //     // We rely on nango to refresh the access token...
-  //     headers: {
-  //       authorization: `Bearer ${settings.oauth.credentials.access_token}`,
-  //     },
-  //     links: (defaultLinks) => [
-  //       (req, next) => {
-  //         if (sdk.clientOptions.baseUrl) {
-  //           req.headers.set(
-  //             nangoProxyLink.kBaseUrlOverride,
-  //             sdk.clientOptions.baseUrl,
-  //           )
-  //         }
-  //         return next(req)
-  //       },
-  //       ...fetchLinks,
-  //       ...defaultLinks,
-  //     ],
-  //   })
-  //   return sdk
-  // },
-  // passthrough: (instance, input) =>
-  //   instance.request(input.method, input.path, {
-  //     headers: input.headers as Record<string, string>,
-  //     params: {query: input.query},
-  //     body: JSON.stringify(input.body),
-  //   }),
+  newInstance: ({settings, fetchLinks}) => {
+    const sdk = initGoogleSDK({
+      // Do we rely on nango to refresh the access token?
+      headers: {
+        authorization: `Bearer ${settings.oauth.credentials.access_token}`,
+      },
+      links: (defaultLinks) => [
+        (req, next) => {
+          if (sdk.clientOptions.baseUrl) {
+            req.headers.set(
+              nangoProxyLink.kBaseUrlOverride,
+              sdk.clientOptions.baseUrl,
+            )
+          }
+          return next(req)
+        },
+        ...fetchLinks,
+        ...defaultLinks,
+      ],
+    })
+    // TODO: should this default to drive_v3?
+    return sdk?.drive_v3
+  },
+  passthrough: (instance, input) =>
+    instance.request(input.method, input.path, {
+      headers: input.headers as Record<string, string>,
+      params: {query: input.query},
+      body: JSON.stringify(input.body),
+    }),
 } satisfies ConnectorServer<typeof googleSchemas>
 
 export default googleServer
