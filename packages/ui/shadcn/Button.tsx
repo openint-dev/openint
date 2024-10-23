39e939b804eccdf956c5c1b736c95549a29ed34b
santiago <snrondina@gmail.com>
2024-10-23 20:44:45 -0300

    category filter

--- packages/ui/shadcn/Button.tsx
+++ packages/ui/shadcn/Button.tsx
@@ -4,24 +4,25 @@ import React from 'react'
 import {cn} from '../utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
+  'inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
   {
     variants: {
       variant: {
-        default: 'text-primary-foreground bg-[#8A5DF6] hover:bg-[#A082E9]',
+        default:
+          'text-button-foreground bg-button hover:bg-button-hover border-button-stroke border',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
         outline:
           'border border-input hover:bg-accent hover:text-accent-foreground',
         secondary:
-          'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+          'bg-button-secondary text-secondary-foreground hover:bg-button-secondary-hover border-button-secondary-stroke border',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'underline-offset-4 hover:underline text-primary',
       },
       size: {
         default: 'h-10 py-2 px-4',
-        sm: 'h-9 px-3 rounded-md',
-        lg: 'h-11 px-8 rounded-md',
+        sm: 'h-8 px-4 rounded-md',
+        lg: 'h-12 px-10 rounded-md',
       },
     },
     defaultVariants: {
